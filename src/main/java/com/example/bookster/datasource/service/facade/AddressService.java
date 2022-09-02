ececf12ce3b00da413367a15238a13a0613fe185
Erik Svensson <eosvensson@gmail.com>
2022-09-02 14:36:55 +0200

    Deleted unneeded classes

--- src/main/java/com/example/bookster/datasource/service/facade/AddressService.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookster.datasource.service.facade;
-
-import com.example.bookster.datasource.service.generic.GenericServiceFacade;
-import com.example.bookster.graphql.models.dto.Address;
-import com.example.bookster.graphql.models.input.AddressInput;
-
-public interface AddressService extends GenericServiceFacade<AddressInput, Address, String> {
-}
--- src/main/java/com/example/bookster/datasource/service/facade/AppUserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookster.datasource.service.facade;
-
-import com.example.bookster.datasource.service.generic.GenericServiceFacade;
-import com.example.bookster.graphql.models.dto.AppUser;
-import com.example.bookster.graphql.models.input.AppUserInput;
-
-public interface AppUserService extends GenericServiceFacade<AppUserInput, AppUser, String> {
-}
